9548e44c9a04f52ca8812b1b1cab22c31b65827f
Eevee5678 <108290506+Eevee5678@users.noreply.github.com>
2023-11-02 16:36:31 -0500

    pid value update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -32,9 +32,9 @@ public class DriveConstants {
      * If using the built-in motor velocity PID, update MOTOR_VELO_PID with the tuned coefficients
      * from DriveVelocityPIDTuner.
      */
-    public static final boolean RUN_USING_ENCODER = false;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
-            getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
+    public static final boolean RUN_USING_ENCODER = true;
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(2.5, 0.1, 0.2,
+            getMotorVelocityF(2350));
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -46,7 +46,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.88976; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 1; // in TODO Find
+    public static double TRACK_WIDTH = 13.16; // in TODO Find
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -68,16 +68,16 @@ public class DriveConstants {
     public static double MAX_VEL = MAX_RPM * (1/60) * GEAR_RATIO * WHEEL_RADIUS *2*Math.PI * 0.7;
     //theoretical, 0.7 is so motors dont go TOO fast
     public static double MAX_ACCEL = 30;//TODO keep raising until path following suffers
-    public static double MAX_ANG_VEL = Math.toRadians(60);//TODO above
+    public static double MAX_ANG_VEL = 9.62;//TODO above
     public static double MAX_ANG_ACCEL = Math.toRadians(60);//TODO only found thru tuning
 
     /*
      * Adjust the orientations here to match your robot. See the FTC SDK documentation for details.
      */
     public static RevHubOrientationOnRobot.LogoFacingDirection LOGO_FACING_DIR =
-            RevHubOrientationOnRobot.LogoFacingDirection.UP;
+            RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD;
     public static RevHubOrientationOnRobot.UsbFacingDirection USB_FACING_DIR =
-            RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+            RevHubOrientationOnRobot.UsbFacingDirection.DOWN;
 
 
     public static double encoderTicksToInches(double ticks) {
