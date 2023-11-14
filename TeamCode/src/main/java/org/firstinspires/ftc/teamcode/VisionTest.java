e7c7bf72a5e146e6c7dd6f4607a67e52f2032cb0
AtticusFinch2 <bradyspymiller71@gmail.com>
2023-11-14 10:34:56 -0600

    2048AI

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionTest.java
@@ -1,14 +1,23 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import org.firstinspires.ftc.teamcode.CSTeleOP;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.firstinspires.ftc.teamcode.TeamMarkerDetection;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 
+class WebCamBot{ //Webcam only, this is for testing purposes
+    HardwareMap map = null;
+    public void init(HardwareMap maps){
+        map = maps;
+    }
+}
 @Autonomous(name = "render Test")
 public class VisionTest extends LinearOpMode {
 
@@ -21,12 +30,13 @@ public class VisionTest extends LinearOpMode {
     // Name of the Webcam to be set in the config
     private String webcamName = "Webcam 1";
     //imports a new robot into the file
-
+    WebCamBot robot = new WebCamBot();
     //motor power constant in our code
     double pwr = 0.35;
 
     @Override
     public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         //this line COULD be jank, shouldn't be
         webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
@@ -41,7 +51,7 @@ public class VisionTest extends LinearOpMode {
             @Override
             public void onOpened()
             {
-                webcam.startStreaming(320,240, OpenCvCameraRotation.SIDEWAYS_LEFT);
+                webcam.startStreaming(640,480, OpenCvCameraRotation.SIDEWAYS_RIGHT);
             }
 
             @Override
@@ -52,14 +62,14 @@ public class VisionTest extends LinearOpMode {
         waitForStart();
         while(opModeIsActive()) {
             sleep(5000);
-
-
+            webcam.stopStreaming();
         }
 
 
         while (!isStarted()) {
             telemetry.addData("ROTATION: ", teamMarkerDetection.getPosition());
             telemetry.update();
+
         }
 
     }
