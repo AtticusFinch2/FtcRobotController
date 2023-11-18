ef544d6c1763767934d4e4003dbea2251041ff64
Eevee5678 <108290506+Eevee5678@users.noreply.github.com>
2023-11-18 09:23:16 -0600

    parkauton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedPark.java
@@ -1,4 +1,58 @@
 package org.firstinspires.ftc.teamcode;
 
-public class RedPark {
+import android.util.Size;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.sun.tools.javac.Main;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.teamcode.Components.MainRobot;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+
+import java.util.List;
+@Autonomous(name = "RedPark", group = "RedWorking")
+public class RedPark extends LinearOpMode {
+    MainRobot robot;
+    int spike;
+    public Pose2d startPose = new Pose2d(24,-70,Math.toRadians(-180));
+    @Override
+    public void runOpMode() {
+
+        robot = new MainRobot(hardwareMap);
+        waitForStart();
+        while (opModeIsActive()) {
+            robot.pause(1200);
+            doTheCvThing();
+            robot.setPoseEstimate(startPose);
+            Trajectory straferight = robot.trajectoryBuilder(startPose)
+                    .strafeRight(28)
+                    .build();
+            startPose = straferight.end();
+
+            robot.followTrajectory(straferight);
+            robot.pause(30000);
+
+        }
+
+    }
+
+    public void doTheCvThing() {
+        robot.visionred.open();
+        robot.pause(100);// hoping this is enough to get the camera booted up
+        spike = robot.visionred.getSpike();
+        robot.visionred.close();
+    }
+
+
 }
