b9831af91ddf1d1a75009e50b3358a1b2b7d5eca
Eevee5678 <108290506+Eevee5678@users.noreply.github.com>
2023-12-06 08:36:39 -0600

    Update RedAuton1.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAuton1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAuton1.java
@@ -25,74 +25,107 @@ import java.util.List;
 public class RedAuton1 extends LinearOpMode {
     MainRobot robot;
     int spike = 2;
-    public Pose2d startPose = new Pose2d(24, 70, Math.toRadians(90));
+    public Pose2d startPose = new Pose2d(24, 70, Math.toRadians(270));
     @Override
     public void runOpMode() {
 
-        robot = new MainRobot(hardwareMap, false);
+        robot = new MainRobot(hardwareMap, true);
         waitForStart();
         robot.servos.Rotator.setPosition(0.24);
         doTheCvThing();
         robot.pause(1200);
         robot.setPoseEstimate(startPose);
-        Trajectory right_1 = robot.trajectoryBuilder(startPose)
-                .strafeRight(14)
-                .build();
-        startPose = right_1.end();
-
-        TrajectorySequence turn_1 = robot.trajectorySequenceBuilder(startPose)
-                .turn(Math.toRadians(90))
-                .build();
-        startPose = turn_1.end();
-
-        Trajectory right_2 = robot.trajectoryBuilder(startPose)
-                .strafeRight(32)
-                .build();
-        startPose = right_2.end();
-
-        robot.followTrajectory(right_1);
-        robot.pause(300);
-        robot.followTrajectorySequence(turn_1);
-        robot.pause(300);
-        robot.followTrajectory(right_2);
-        robot.pause(300);
+        spike = 2; // TODO: REMOVE WHEN DONE MAKING TRAJECTORIES FOR EACH SPIKE
+        switch (spike){
+            case 1:
+                //doSpike1(); /**DO NOT RUN*/
+            case 2:
+                doSpike2();
+            case 3:
+                //doSpike3(); /**DO NOT RUN*/
+        }
         pixel1();
-        parkl();
-
-
-
-
+        parkr();
 
     }
-
-
-
-
-
-    public void pixel1(){
-        robot.setPoseEstimate(startPose);
-        Trajectory creepforward = robot.trajectoryBuilder(startPose)
-                .forward(6)
+    /*public void doSpike1(){// TODO
+        Trajectory forward_1  = robot.trajectoryBuilder(startPose)
+                .forward(5)
                 .build();
-        startPose = creepforward.end();
-
+        startPose = forward_1.end();
+        Trajectory spline_1  = robot.trajectoryBuilder(startPose)
+                .splineTo(new Vector2d(startPose.getX()-5, startPose.getY()+15), startPose.getHeading()-Math.toRadians(90))
+                .build();
+        startPose = spline_1.end();
+        Trajectory backup  = robot.trajectoryBuilder(startPose)
+                .back(28)
+                .build();
+        startPose = backup.end();
+        robot.followTrajectory(forward_1);
+        robot.followTrajectory(spline_1);
+        robot.servos.Purps.setPosition(0);
+        robot.followTrajectory(backup);
+        robot.pause(500);
+    }*/
+    public void doSpike2(){
+        Trajectory forward_1  = robot.trajectoryBuilder(startPose)
+                .forward(5)
+                .build();
+        startPose = forward_1.end();
+        Trajectory spline_1  = robot.trajectoryBuilder(forward_1.end())
+                .splineTo(new Vector2d(startPose.getX()+5, startPose.getY()+21), Math.toRadians(0))
+                .build();
+        startPose = spline_1.end();
+        Trajectory backup  = robot.trajectoryBuilder(startPose)
+                .back(38)
+                .build();
+        startPose = backup.end();
+        robot.followTrajectory(forward_1);
+        robot.followTrajectory(spline_1);
+        robot.servos.Purps.setPosition(0);
+        robot.followTrajectory(backup);
+        robot.pause(500);
+    }/*
+    public void doSpike3(){// TODO
+        Trajectory forward_1  = robot.trajectoryBuilder(startPose)
+                .forward(5)
+                .build();
+        startPose = forward_1.end();
+        Trajectory spline_1  = robot.trajectoryBuilder(startPose)
+                .splineTo(new Vector2d(startPose.getX()+5, startPose.getY()+15), startPose.getHeading()+Math.toRadians(180))
+                .build();
+        startPose = spline_1.end();
+        Trajectory backup  = robot.trajectoryBuilder(startPose)
+                .back(33)
+                .build();
+        startPose = backup.end();
+        robot.followTrajectory(forward_1);
+        robot.followTrajectory(spline_1);
+        robot.servos.Purps.setPosition(1);
+        robot.followTrajectory(backup);
+        robot.pause(500);
+    }*/
+    public void pixel1() {
         Trajectory creepbackward = robot.trajectoryBuilder(startPose)
-                .back(6)
+                .back(10)
                 .build();
         startPose = creepbackward.end();
-
+        Trajectory creepforward = robot.trajectoryBuilder(startPose)
+                .forward(8)
+                .build();
+        startPose = creepforward.end();
 
         robot.slides.setSlidesPower(1.0);
         robot.pause(1000);
         robot.servos.Backhand.setPosition(0.5); //open
-        robot.pause(300);
-        robot.servos.Rotator.setPosition(0.48);
-        robot.pause(400);
-        robot.followTrajectory(creepbackward);
         robot.pause(1000);
+        robot.servos.Rotator.setPosition(0.48);
+        robot.pause(1000);
+        robot.followTrajectory(creepbackward);
+        robot.pause(2000);
         robot.servos.Dropper.setPosition(0.3);
         robot.pause(500);
-        robot.followTrajectory(creepforward);
+        //robot.followTrajectory(creepforward);
         robot.pause(1000);
         robot.servos.Dropper.setPosition(0.7);
         robot.pause(200);
@@ -104,17 +137,17 @@ public class RedAuton1 extends LinearOpMode {
         robot.pause(1200);
         robot.slides.setSlidesPower(0);
     }
-    public void parkl(){
-        Trajectory leftpark = robot.trajectoryBuilder(startPose)
-                .strafeLeft(32)
+    public void parkr(){
+        Trajectory rightpark = robot.trajectoryBuilder(startPose)
+                .strafeRight(32)
                 .build();
-        startPose = leftpark.end();
+        startPose = rightpark.end();
 
         Trajectory creepbackward2 = robot.trajectoryBuilder(startPose)
                 .back(6)
                 .build();
         startPose = creepbackward2.end();
-        robot.followTrajectory(leftpark);
+        robot.followTrajectory(rightpark);
         robot.pause(300);
         robot.followTrajectory(creepbackward2);
     }
